ab61aebc6364d4e8252875db831b88b74dc98e6f
mohammed-rharbi <rharbi383@gmail.com>
2025-01-21 22:14:48 +0100

    ⚽ Add team module setup

---
+++ backend/src/team/entities/team.entity.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema ,SchemaFactory } from "@nestjs/mongoose";
+import { Document , Types } from "mongoose";
+
+
+const logo = 'https://i.pinimg.com/736x/62/1e/6f/621e6f89c5161b72adbedda3edc2d29c.jpg';
+
+@Schema()
+export class Team extends Document {
+
+
+    @Prop({required:true})
+    name: string ;
+
+
+    @Prop({default: logo})
+    logo: string ;
+
+    
+    @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+    captain: Types.ObjectId;
+
+    
+    @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
+    members: Types.ObjectId[];
+    
+
+    @Prop({ enum:['2-2' , '3-1' , '2-1-1', '1-2-1'] , default: "3-1" }) 
+    formation: string;
+
+
+    @Prop({ default: Date.now })
+    createdAt: Date; 
+
+}
+
+export const TeamSchema = SchemaFactory.createForClass(Team)
