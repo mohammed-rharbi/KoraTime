48705a1238edcf39a21aa9ef3039a8d619f7f879
mohammed-rharbi <rharbi383@gmail.com>
2025-01-21 22:14:38 +0100

    📦 Update DTOs for authentication requests

--- backend/src/auth/dto/create-auth.dto.ts
+++ backend/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,21 @@
-export class CreateAuthDto {}
+import { Type } from "class-transformer";
+import { IsNotEmpty , IsString , IsDate, IsOptional } from "class-validator";
+
+
+export class CreateUserDto {
+
+@IsString()
+@IsNotEmpty()
+name: string ;
+
+@IsString()
+@IsNotEmpty()
+captain: string;
+
+@IsOptional()
+@IsString()
+logo? : string ;
+
+
+
+}
