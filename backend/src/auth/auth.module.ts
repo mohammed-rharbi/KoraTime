2d4562fd4af8f60327bf39c01610abcd5c000b7b
mohammed-rharbi <rharbi383@gmail.com>
2025-01-21 22:14:34 +0100

    🔑 Enhance authentication module setup

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User , UserSchema } from './entities/auth.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [ MongooseModule.forFeature([{ name: User.name , schema: UserSchema }])],
   controllers: [AuthController],
   providers: [AuthService],
 })
