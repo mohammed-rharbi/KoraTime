b204ba53a3394157f23cd19606b0a09207d33a11
mohammed-rharbi <rharbi383@gmail.com>
2025-01-22 10:24:31 +0100

    feat: 🗄️ Add authentication repository layer

---
+++ backend/src/auth/auth.repository.ts
@@ -0,0 +1,53 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { User  } from "./entities/auth.entity";
+import { CreateUserDto } from "./dto/create-auth.dto";
+import { UpdateUserDto } from "./dto/update-auth.dto";
+
+
+
+@Injectable()
+
+export class AuthRepository {
+
+    constructor(@InjectModel(User.name) private readonly UserModel : Model<User> ){}
+
+
+    async create(userData: CreateUserDto ): Promise<User>{
+
+        const user =  new this.UserModel(userData);
+        return user.save();
+
+    }
+    
+    async findAll() : Promise<User[]>{
+
+        return await this.UserModel.find({role: 'participant'}).exec();
+    }
+
+
+    async findByEmail (email: string) : Promise<User>{
+
+        return await this.UserModel.findOne({email}).exec();
+    }
+
+
+    async findById(id : string): Promise<User>{
+
+        return await this.UserModel.findOne({_id: id}).exec();
+
+    }
+
+    async update(id: string , userData: UpdateUserDto): Promise<User>{
+
+        return await this.UserModel.findByIdAndUpdate(id , userData , {new: true}).exec();
+    }
+
+
+    async delete(id: string): Promise<User>{
+        
+        return await this.UserModel.findByIdAndDelete(id).exec();
+    }
+
+}
