9a10abbb7be03ffd345126858c2b5be800b40ac5
mohammed-rharbi <rharbi383@gmail.com>
2025-01-22 17:28:23 +0100

    chore: 🔧 Refactor authentication module structure

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,13 +5,14 @@ import { User , UserSchema } from './entities/auth.entity';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthRepository } from './auth.repository';
 import { JwtModule } from '@nestjs/jwt';
+import "dotenv/config"
 
 @Module({
   imports: [ 
     
     MongooseModule.forFeature([{ name: User.name , schema: UserSchema }]) , 
 
-    JwtModule.register({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '3h'}})           
+    JwtModule.register({ secret: process.env.JWT_SECRET , signOptions: { expiresIn: '3h'}})           
 ],
 
     controllers: [AuthController],
