6a00c98bb0d995cd3a9868b8ade703194f8729d5
mohammed-rharbi <rharbi383@gmail.com>
2025-01-22 15:43:31 +0100

    feat: 🚀 Improve application bootstrap logic

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen( process.env.PORT ?? 3000 , ()=>{
+
+    console.log(`this app is starting on port ${process.env.PORT}`);
+    
+  });
 }
 bootstrap();
