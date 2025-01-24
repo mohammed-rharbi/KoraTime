832cde87f1dcb8e5ead88d770fec1056d81422ea
mohammed-rharbi <rharbi383@gmail.com>
2025-01-24 09:01:01 +0100

    feat(auth): enhance authentication service logic

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-auth.dto';
 import { UpdateUserDto } from './dto/update-auth.dto';
 import { AuthRepository } from './auth.repository';
@@ -18,6 +18,7 @@ export class AuthService {
       const {firstName , lastName , email , password} = UserData
 
       const existingUser = await this.AuthRepository.findByEmail(email);
+      
       if (existingUser) {
         throw new UnauthorizedException('User already exists');
       }
@@ -28,13 +29,11 @@ export class AuthService {
         ...UserData,
         password: hashPass,
       };
-
-      console.log(newUser);
   
-      await this.AuthRepository.create(newUser);
+      const user =  await this.AuthRepository.create(newUser);
       
       
-      return { message: 'User registered successfully' };
+      return { message: 'User registered successfully' , user };
 
     } catch (err: any ) {
       console.error('Registration error:', err.message);
@@ -74,6 +73,27 @@ export class AuthService {
       
     }
   
+  }
+  async getstarted(phoneNumber: string, profilePic: string, id: string) {
+
+    if (!phoneNumber || !profilePic || !id) {
+      throw new BadRequestException('All fields are required.');
+    }
+  
+    const userData = {
+      profilePic,
+      phoneNumber,
+    };
+  
+    try {
+
+      const updatedUser = await this.AuthRepository.update(id, userData);
+  
+      return updatedUser;
+    } catch (err) {
+      console.error(err);
+      throw new InternalServerErrorException('Failed to update user profile');
+    }
   }
 
 }
