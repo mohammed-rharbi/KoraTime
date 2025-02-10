c607eada2825a5b54c51e7fdeecf61da4f502004
mohammed-rharbi <rharbi383@gmail.com>
2025-02-10 11:48:05 +0100

    feat(gateway): add new gateway module

---
+++ backend/src/gateway/gateway.gateway.ts
@@ -0,0 +1,8 @@
+import { WebSocketGateway } from '@nestjs/websockets';
+import { GatewayService } from './gateway.service';
+
+@WebSocketGateway()
+export class GatewayGateway {
+  
+  constructor(private readonly gatewayService: GatewayService) {}
+}
