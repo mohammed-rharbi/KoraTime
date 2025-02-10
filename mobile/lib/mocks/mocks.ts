878f34e957ba51dfde4a05cd7f0bd934256a2442
mohammed-rharbi <rharbi383@gmail.com>
2025-02-10 11:48:07 +0100

    feat(lib): add utility functions

---
+++ mobile/lib/mocks/mocks.ts
@@ -0,0 +1,28 @@
+
+
+
+export const pitches = [
+    
+    {
+        id:1,
+        name: 'City Sports Arena',
+        image: 'https://i.pinimg.com/736x/31/e9/e2/31e9e2b3edb0624892e0b0718b49e0ba.jpg',
+        des:'2.8K',
+        rate: 4.5
+    },
+    {
+        id:2,
+        name: 'La Rissa Arena',
+        image: 'https://i.pinimg.com/474x/16/9c/cd/169ccd0945c1c2b939a872da6bd99949.jpg',
+        des:'1.8K',
+        rate: 4.8
+    },
+    {
+        id:3,
+        name: 'Dar Chabab',
+        image: 'https://i.pinimg.com/474x/e4/10/c9/e410c96eb6813923994d6c0a8a57d7db.jpg',
+        des:'4.5K',
+        rate: 3.8
+    },
+
+]
