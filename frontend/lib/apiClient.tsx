4f8b17cdb7f395f3ac113cb7dbd0892448ea3fe4
mohammed-rharbi <rharbi383@gmail.com>
2025-02-25 17:35:29 +0100

    chore: add utility libraries

---
+++ frontend/lib/apiClient.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+
+const apiClient = axios.create({
+    baseURL: process.env.API,
+});
+
+apiClient.interceptors.request.use(
+    
+    async (config)=> {
+
+        const token = localStorage.getItem('token');    
+   
+        config.headers['Content-Type'] = 'application/json'
+        config.headers.Authorization = `Bearer ${token}`;
+    
+        return config;
+      },
+
+        (error)=>{
+
+        return Promise.reject(error); 
+      }
+
+)
+
+export default apiClient;
