ac4148c2616e2060b8316b2826aca2efb68b76da
mohammed-rharbi <rharbi383@gmail.com>
2025-03-03 15:11:38 +0100

    chore(config): update Next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['http://127.0.0.1:9000'],
+  },
 };
 
 export default nextConfig;
