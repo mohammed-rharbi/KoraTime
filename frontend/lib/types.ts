1bf9aa28cca2fb0b52b1959f48b45947096b05f4
mohammed-rharbi <rharbi383@gmail.com>
2025-03-03 15:11:38 +0100

    refactor(types): update type definitions

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -14,3 +14,18 @@ export interface LoginType {
    }
    
   
+
+   export interface FieldType {
+    _id: string,
+    name: string,
+    description: string,
+    location: string,
+    fieldManger: string,  
+    price: string,
+    size: string,
+    lightsAvailable: boolean,
+    IsAvailable: boolean,
+    status: string,
+    photo: string | null,
+
+   }
