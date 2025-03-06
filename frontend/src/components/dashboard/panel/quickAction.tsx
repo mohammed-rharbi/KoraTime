0021abbd990bd636beb44ca0ee2803bc08eff82e
mohammed-rharbi <rharbi383@gmail.com>
2025-03-06 13:52:45 +0100

    feat(ui): update quick action panel

--- frontend/src/components/dashboard/panel/quickAction.tsx
+++ frontend/src/components/dashboard/panel/quickAction.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 
 export default function QuickAction() {
@@ -7,22 +8,22 @@ export default function QuickAction() {
         <div className="bg-gray-800/50 p-6 rounded-xl border border-gray-700/50">
             <h3 className="text-xl font-bold mb-4">Quick Actions</h3>
             <div className="space-y-4">
-              <button className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
+              <Link href={'/dashboard/fields'} className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
                 <span>🏟️</span>
                 Manage Fields
-              </button>
-              <button className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
+              </Link>
+              <Link href={'/dashboard/fieldManagers'} className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
                 <span>👔</span>
                 Manage Managers
-              </button>
-              <button className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
+              </Link>
+              <Link href={'/dashboard/teams'} className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
                 <span>👥</span>
                 View Teams
-              </button>
-              <button className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
+              </Link>
+              <Link href={'/dashboard/players'} className="w-full text-left p-4 rounded-lg bg-gray-700/30 hover:bg-gray-700/50 transition-colors flex items-center gap-3">
                 <span>📋</span>
                 Manage Players
-              </button>
+              </Link>
             </div>
         </div>
   )
