85cd34de45c5d93975a36ab155925b164dbfafad
mohammed-rharbi <rharbi383@gmail.com>
2025-03-06 09:50:46 +0100

    feat(api): update authentication API

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,4 +1,5 @@
 import apiClient from "../../lib/apiClient";
+import { ManagerType } from "../../lib/types";
 
 
 
@@ -15,3 +16,28 @@ export const loginUser = async (email: string , password:string) => {
     throw new Error('Login failed. Please check your credentials.');
   }
 };
+
+export const createManager = async (ManagerData:ManagerType) => {
+
+  try {
+    const response = await apiClient.post(`auth/createManger`,  ManagerData );
+    
+    return response.data
+
+  } catch (error) {
+    throw new Error('failed to create a manager. Please check the credentials.');
+  }
+};
+
+export const getAllManagers = async () => {
+
+  try {
+    const response = await apiClient.get(`auth/getAllManagers`);
+    
+    return response.data
+
+  } catch (error) {
+    throw new Error('failed to fetch the managers.');
+  }
+};
+
