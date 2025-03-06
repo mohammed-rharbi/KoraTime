c790f52859b5e92c550dff9616c33c4b0fe5008a
mohammed-rharbi <rharbi383@gmail.com>
2025-03-06 13:52:46 +0100

    feat(state): update manager store logic

--- frontend/store/managerStore.ts
+++ frontend/store/managerStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { ManagerType } from "../lib/types";
 import { createManager , getAllManagers } from "@/api/authApi";
+import { banUser } from "@/api/adminApi";
 
 
 
@@ -10,6 +11,7 @@ interface AuthState {
     fieldManagers: ManagerType[] | null ;
     AddManager: (ManagerData: ManagerType)=> Promise<ManagerType>;
     getManagers: ()=> Promise<void>
+    banAUser: (id:string , action: "ban" | "unban")=> Promise<void>
     
   }
   
@@ -20,7 +22,7 @@ const useManagerStore = create<AuthState>((set)=>({
     error: null,
     fieldManagers: null,
 
-    AddManager: async (ManagerData: ManagerType)=>{
+    AddManager: async (ManagerData)=>{
 
         set({isLoading:true , error:null})
 
@@ -43,17 +45,28 @@ const useManagerStore = create<AuthState>((set)=>({
        try {
 
             const res = await getAllManagers()
-            set({fieldManagers:res})
+            set({fieldManagers:res , isLoading:false})
                 
         } catch (err) {
             set({error:(err as Error).message , isLoading:false })
             
-        }finally{
-
-            set({isLoading:false})
         }
-    }
-
+    },
+    banAUser: async (id: string, action: "ban" | "unban") => {
+        set((state) => ({ ...state, isLoading: true, error: null }));
+      
+        try {
+          await banUser(id, action);
+          set((state) => ({ ...state, isLoading: false }));
+        } catch (err) {
+          set((state) => ({
+            ...state,
+            error: err instanceof Error ? err.message : "An unknown error occurred",
+            isLoading: false,
+          }));
+        }
+      }
+      
 }))
 
 
