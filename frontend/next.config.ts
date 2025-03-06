4d75271763bc28b9276aff20641e685a6fd294eb
mohammed-rharbi <rharbi383@gmail.com>
2025-03-06 09:50:45 +0100

    chore(config): update Next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
   images: {
-    domains: ['http://127.0.0.1:9000'],
-  },
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: '*',
+            port: '*',
+            pathname: '*',
+        },
+    ],
+},
 };
 
 export default nextConfig;
