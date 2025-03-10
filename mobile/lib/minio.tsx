325f5c3da26a467e5d4c4f1aa0a9907e64b82ae8
mohammed-rharbi <rharbi383@gmail.com>
2025-03-10 15:45:24 +0100

    chore(storage): add MinIO integration for mobile

---
+++ mobile/lib/minio.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const uploadImageToBackend = async (imageUri: string): Promise<string> => {
+  try {
+
+    const response = await fetch(imageUri);
+    const blob = await response.blob();
+    
+
+    const file = new File([blob], 'product-image.jpg', { type: blob.type });
+
+
+    const formData = new FormData();
+    formData.append('file', file);  
+  
+
+    const { data } = await axios.post(`http://localhost:4000/upload`,
+      formData
+    );
+
+    return data.imageUrl;
+  } catch (error) {
+    console.error('Image upload failed:', error);
+
+    return 'https://i.pinimg.com/736x/70/8c/08/708c08614099f90b849c6f7089f8effb.jpg';
+  }
+};
