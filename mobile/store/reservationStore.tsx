7ab311bb09bc458596feafe30381f20ca62c37a0
mohammed-rharbi <rharbi383@gmail.com>
2025-03-12 10:29:55 +0100

    fix(store): update reservation store logic

--- mobile/store/reservationStore.tsx
+++ mobile/store/reservationStore.tsx
@@ -1,18 +1,21 @@
 import { create } from "zustand";
-import { BookField } from "~/api/reservation.api";
+import { BookField , getAllUserReservations } from "~/api/reservation.api";
 import { ReservationType } from "~/types/types";
 
 
-interface FieldState {
+interface ReservationState {
     isLoading: boolean;
     error: string | null;
+    userReservations: ReservationType[] | null;
     bookIt: (bookingData:ReservationType)=> Promise<void>;
+    getUserReservations: (id: string)=> Promise<void>
   }
   
-const ReservationStore = create<FieldState>((set)=>({
+const ReservationStore = create<ReservationState>((set)=>({
 
     isLoading: false,
     error: null,
+    userReservations: null,
 
   
     bookIt: async (data)=>{
@@ -21,6 +24,18 @@ const ReservationStore = create<FieldState>((set)=>({
         try {
             const res = await BookField(data);
             set({isLoading:false})
+            
+        } catch (err){
+            set({error:(err as Error).message , isLoading:false})            
+        }
+    },
+
+    getUserReservations: async (id)=>{
+
+        set({isLoading:true , error:null}); 
+        try {
+            const res = await getAllUserReservations(id);
+            set({isLoading:false , userReservations:res})
             
         } catch (err){
             set({error:(err as Error).message , isLoading:false})            
