757b8909f6ce1e607e8cb3aa2292fc52562751d6
mohammed-rharbi <rharbi383@gmail.com>
2025-03-15 14:54:57 +0100

    Refactored FriendshipController for better modularity and maintainability

--- backend/src/friendship/friendship.controller.ts
+++ backend/src/friendship/friendship.controller.ts
@@ -40,14 +40,4 @@ export class FriendshipController {
   }
   
 
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateFriendshipDto: UpdateFriendshipDto) {
-  //   return this.friendshipService.update(+id, updateFriendshipDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.friendshipService.remove(+id);
-  // }
 }
