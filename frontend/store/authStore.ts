ed7b9e67721b17ae24fa7fc3aae589b909049308
mohammed-rharbi <rharbi383@gmail.com>
2025-03-19 10:29:52 +0100

    Updated authentication store logic.

--- frontend/store/authStore.ts
+++ frontend/store/authStore.ts
@@ -3,9 +3,6 @@ import { loginUser } from "@/api/authApi";
 import { UserType } from "../lib/types";
 
 
-
-
-
 interface AuthState {
     user: UserType | null;
     token: string | null;
@@ -17,42 +14,42 @@ interface AuthState {
   }
   
 
-const useAuthStore = create<AuthState>((set)=>({
+const useAuthStore = create<AuthState>((set) => {
 
-    user: null ,
-    token: null ,
-    isLoading: false,
-    error: null,
-    role: null,
-
-
-    login: async (email, password)=>{
-
-        set({isLoading:true , error:null });
-
-        try{
-
-            const res = await loginUser(email , password);  
-            set({user: res.user , token: res.token , isLoading:false , role:res.user.role})
-            localStorage.setItem('AdminToken' , res.token)
-            localStorage.setItem('AdminRole' , res.user.role)
-
-            
-            
-        }catch(err){
-            set({error:(err as Error).message , isLoading:false})
-        }
-    },
-
-
-    logout: async ()=>{
-     set({user:null , token: null , role:null })
-     localStorage.removeItem('AdminToken')
-     localStorage.removeItem('AdminRole')
-
+    let initialToken: string | null = null;
+    let initialRole: string | null = null;
+    
+    if (typeof window !== 'undefined') {
+      initialToken = localStorage.getItem('AdminToken');
+      initialRole = localStorage.getItem('AdminRole');
     }
-
-}))
+  
+    return {
+      user: null,
+      token: initialToken,
+      role: initialRole,
+      isLoading: false,
+      error: null,
+      login: async (email, password) => {
+        set({ isLoading: true, error: null });
+        try {
+          const res = await loginUser(email, password);
+          set({ user: res.user, token: res.token, role: res.user.role });
+          localStorage.setItem('AdminToken', res.token);
+          localStorage.setItem('AdminRole', res.user.role);
+        } catch (err) {
+          set({ error: (err as Error).message });
+        } finally {
+          set({ isLoading: false });
+        }
+      },
+      logout: () => {
+        set({ user: null, token: null, role: null });
+        localStorage.removeItem('AdminToken');
+        localStorage.removeItem('AdminRole');
+      }
+    };
+  });
 
 
 export default useAuthStore
